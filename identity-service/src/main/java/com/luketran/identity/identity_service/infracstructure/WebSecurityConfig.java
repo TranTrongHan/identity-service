2b10523367095a97b075712bac032a05bfc7b2f7
TranTrongHan <hintran6@gmail.com>
2026-06-10 12:14:07 +0700

    init project

---
+++ identity-service/src/main/java/com/luketran/identity/identity_service/infracstructure/WebSecurityConfig.java
@@ -0,0 +1,7 @@
+package com.luketran.identity.identity_service.infracstructure;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class WebSecurityConfig {
+}
